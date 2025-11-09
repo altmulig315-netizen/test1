c79cb9e6e2f1ef22b3a23c74450b912ebaaeaf7d
altmulig315-netizen <altmulig315@gmail.com>
2025-11-09 21:08:41 +0100

    first commit

---
+++ Test.js
@@ -0,0 +1,19 @@
+console.log("               WILLIAMS GRILL")
+console.log("---------------------------------------------")
+console.log("               Order Details")
+
+console.log("---------------------------------------------")
+console.log(" Item,            Quantity,             Price")
+
+console.log("---------------------------------------------")
+
+console.log("Burger                1                 3")
+console.log("Soda                  1                 2")
+console.log("Fries                 1                 4")
+
+console.log("---------------------------------------------")
+console.log("                     Total              9")
+
+console.log("=============================================")
+
+// Det er mange måter å gjøre ting på
